bb08bd805cb8016e5ab2fe701faa9ea341882e65
David Whitney <codifyamplify@gmail.com>
2022-06-24 15:09:04 -0400

    commit tree 0d2c10281e6b4c48389508f16a36ff3af106be03

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+const employee = {
+    name: "david whitney",
+    streetAdress: "1107 pine grove drive"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const updatedObj = {...employee};
+    updatedObj[key] = value;
+    return updatedObj
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value
+    return employee
+}
+function deleteFromEmployeeByKey(employee, key){
+    // const copyOfEmployee = {...employee}
+    // return delete copyOfEmployee[key]
+    const newObj = { ...employee }
+    delete newObj[key]
+    return newObj
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+    return employee
+}
